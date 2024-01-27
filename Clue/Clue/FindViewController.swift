2f8e472c4814713fa1c82824667942f923cad2b4
kimsunho2000 <sunhokim28@gmail.com>
2024-01-27 16:34:26 +0900

    fixed autolayout and add sign up UI

---
+++ Clue/Clue/FindViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FindViewController.swift
+//  Clue
+//
+//  Created by 김선호 on 1/27/24.
+//
+
+import UIKit
+
+class FindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Clue/Clue/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  Clue
+//
+//  Created by 김선호 on 1/27/24.
+//
+
+import UIKit
+
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var signupLabel: UILabel!
+    @IBOutlet weak var inputIDTextfield: UITextField!
+    @IBOutlet weak var inputPWTextfield: UITextField!
+    @IBOutlet weak var errormessageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
